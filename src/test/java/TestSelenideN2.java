f7b4417c907f9c01aa6ce570ee73bf8d4d40841a
vesstar <vesstar.dj@gmail.com>
2023-06-04 18:23:59 +0300

    Selenide hover test

---
+++ src/test/java/TestSelenideN2.java
@@ -0,0 +1,21 @@
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestSelenideN2 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void enterpriseHoveTest() {
+        open("/");
+        $(byText("Solutions")).hover().parent().$("div.HeaderMenu-dropdown").$(byText("Enterprise")).click();
+        $("h1").shouldHave(text("Build like the best"));
+    }
+}
